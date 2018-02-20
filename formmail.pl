d0348811cd84d8f97bef19a1e460a86aa6b2dce9
DESKTOP-12S2DSQ\cathy <rinacathy02@gmail.com>
2018-02-20 14:21:47 +1100

    First Website

---
+++ formmail.pl
@@ -0,0 +1,4 @@
+#!/usr/bin/perl -- Add two hyphens after 'perl'
+$mailprog = '/usr/sbin/sendmail -oi -t';
+@referers = qw(http://users.tpg.com.au/rinacathy02/); ensures emails delivered only to this domain
+@recipients = qw(rinacathy02@tpg.com.au); recipient's email address 
